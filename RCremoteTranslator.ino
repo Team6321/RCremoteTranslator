7f4519bd43b11f493bb469d2d753986afe13e44c
zeta(2) <riemannzeta0@gmail.com>
2018-10-27 17:33:34 -0500

    Added setPulsesOut() and TranslateArcadeToTank()

--- RCremoteTranslator.ino
+++ RCremoteTranslator.ino
@@ -6,8 +6,8 @@ See Repo: https://github.com/Team6321/RCremoteTranslator
 #include<TimerOne.h>  // Avaiable from http://www.arduino.cc/playground/Code/Timer1
 #define THROTTLE_PIN 2
 #define STEERING_PIN 3
-#define LEFT_PIN 4
-#define RIGHT_PIN 5
+#define Left_PIN 4
+#define Right_PIN 5
 
 volatile int Left;
 volatile int Right;
@@ -18,13 +18,20 @@ int freqStep = 8; // This is the delay step in Microseconds
 // It is calculated based on the period of the input pulse (1ms - 2 ms)
 // and the number of speed steps you want (255). 
 
+
+struct scalePulses {
+  float Left; 
+  float Right;
+};
+
+
 void setup() {
   // set pin modes
   pinMode(THROTTLE_PIN, INPUT);
   pinMode(STEERING_PIN, INPUT);
 
-  pinMode(LEFT_PIN, OUTPUT);
-  pinMode(RIGHT_PIN, OUTPUT);
+  pinMode(Left_PIN, OUTPUT);
+  pinMode(Right_PIN, OUTPUT);
   
   // register timer
   
@@ -34,10 +41,44 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-}
-
-void setPulseOut() {
   
+  scalePulses scaledValues = setPulseOut();
+
+  TranslateArcadeToTank(scaledValues.Left, scaledValues.Right);
 }
 
+struct scalePulses setPulseOut() {
+  float steeringDuration, throttleDuration;
+  float scaledSteeringDuration, scaledThrottleDuration;
+  
+  steeringDuration = pulseIn(STEERING_PIN, HIGH);
+  throttleDuration = pulseIn(THROTTLE_PIN, HIGH);
+
+  scaledSteeringDuration = map(steeringDuration, 1000, 2000, -1, 1);
+  scaledThrottleDuration = map(throttleDuration, 1000, 2000, -1, 1);
+
+  struct scalePulses scaledValues = {scaledSteeringDuration, scaledThrottleDuration};
+
+  return scaledValues;
+}
+
+void TranslateArcadeToTank(int x, int y)
+{
+  float throttle = abs(y);
+  float steering = abs(x);
+
+  if (x > 0)
+  {
+    Left = throttle - steering;
+    Right = max(throttle,steering);
+  }
+  else
+  {
+    Left = max(throttle,steering);
+    Right = throttle - steering;
+  }
+
+  float temp = Left;
+  Left = (y < 0)? -1 * Right : Left;
+  Right = (y < 0)? -1 * Right : Right;
+}
