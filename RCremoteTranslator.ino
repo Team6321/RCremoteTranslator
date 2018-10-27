a95bc8073f369d75c0c51d07ff5078169078dc8b
Matthew.Shafer.Laptop <mshafer1@harding.edu>
2018-10-27 16:58:59 -0500

    defining pin numbers

--- RCremoteTranslator.ino
+++ RCremoteTranslator.ino
@@ -3,14 +3,41 @@ This is code to take an RC remote's throttle and steering pulses, translate them
 
 See Repo: https://github.com/Team6321/RCremoteTranslator
 */
+#include<TimerOne.h>  // Avaiable from http://www.arduino.cc/playground/Code/Timer1
+#define THROTTLE_PIN 2
+#define STEERING_PIN 3
+#define LEFT_PIN 4
+#define RIGHT_PIN 5
 
+volatile int Left;
+volatile int Right;
+volatile int Left_Count;
+volatile int Right_Count;
+
+int freqStep = 8; // This is the delay step in Microseconds
+// It is calculated based on the period of the input pulse (1ms - 2 ms)
+// and the number of speed steps you want (255). 
 
 void setup() {
-  // put your setup code here, to run once:
+  // set pin modes
+  pinMode(THROTTLE_PIN, INPUT);
+  pinMode(STEERING_PIN, INPUT);
 
+  pinMode(LEFT_PIN, OUTPUT);
+  pinMode(RIGHT_PIN, OUTPUT);
+  
+  // register timer
+  
+
+  // register pulse out pin (hw interrupt)
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+void setPulseOut() {
+  
+}
+
